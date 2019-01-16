eb7779f6251eb8af1f99eea352eaf87becb88232
unknown <git add --all>
2019-01-16 22:09:45 +0400

    Updates

---
+++ precache-manifest.2c445abeffda025fba789a05d04df888.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "295081262442e1881304",
+    "url": "/vidly/static/css/main.0505bac6.chunk.css"
+  },
+  {
+    "revision": "295081262442e1881304",
+    "url": "/vidly/static/js/main.29508126.chunk.js"
+  },
+  {
+    "revision": "4127d621b6201410b719",
+    "url": "/vidly/static/css/1.16260ea6.chunk.css"
+  },
+  {
+    "revision": "4127d621b6201410b719",
+    "url": "/vidly/static/js/1.4127d621.chunk.js"
+  },
+  {
+    "revision": "ab00d1fec4a6e13b7ab4",
+    "url": "/vidly/static/js/runtime~main.ab00d1fe.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e267029f50f466d5dfe0a76f4e8560fc",
+    "url": "/vidly/index.html"
+  }
+];
